b3de5b4adaa5c0ee2abdb3d7d71e3b89efbb12a4
tobiillz <ayoadeopeoluwa@yahoo.com>
2022-02-07 02:33:28 +0100

    completed journal

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,11 +6,15 @@ export default function Cards(props) {
   return <div className='card-group'>
           <img src={props.item.img} alt="various-locations"  className='card-image'/>
           <div className='card-text'>
-                <span>
-                  <img src="../images/fill219.png" alt="location" />
-                  {props.item.location}
-                  <a href="#">Views on Google map</a>
+                <span className='top-text'>
+                  <img className="location-icon" src="../images/fill219.png" alt="location" />
+                  {props.item.country}
+                  <a href="#" className='links'>Views on Google map</a>
                 </span>
+                <h1 className='location'>{props.item.location}</h1>
+                <b className='date'>{props.item.date}</b>
+                <p className='description'>{props.item.description}</p>
+
           </div>
   </div>;
 }
